b653ef52d0bdcedc9af2253ab4e6662d1772d37e
Vladislav <vintorez.g@gmail.com>
2018-03-26 01:57:13 +0300

    парсинг модели

--- SWD.API/SWD.Domain/Models/SwaggerProperty.cs
+++ SWD.API/SWD.Domain/Models/SwaggerProperty.cs
@@ -1,11 +1,19 @@
-﻿namespace SWD.Domain.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SWD.Domain.Models
 {
     public class SwaggerProperty
     {
-        public string Key { get; set; }
+        public string Name { get; set; }
+
+        [JsonProperty("in")]
+        public string Source { get; set; }
 
         public string Format { get; set; }
 
         public string Description { get; set; }
+
+        public bool Required { get; set; }
     }
 }
--- SWD.API/SWD.Domain/SwaggerSchemeProcessor.cs
+++ SWD.API/SWD.Domain/SwaggerSchemeProcessor.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 using SWD.Domain.Models;
 
 namespace SWD.Domain
@@ -9,9 +11,32 @@ namespace SWD.Domain
         {
             var jObject = JObject.FromObject(jsonScheme);
 
+            var actions = new List<ActionDefinition>();
+            foreach (var path in jObject["paths"].Children())
+            {
+                var url = ((JProperty) path).Name;
+                foreach (var methods in path.Values())
+                {
+                    foreach (var method in methods)
+                    {
+                        var type = ((JProperty) methods).Name;
+                        var description = method["summary"].Value<string>();
+                        var properties = method["parameters"];
+                        actions.Add(new ActionDefinition
+                        {
+                            Url = url,
+                            Type = type,
+                            Description = description,
+                            Properties = properties.ToObject<List<SwaggerProperty>>()
+                        });
+                    }
+                }
+            }
+
             return new SwaggerApiModel
             {
-                Title = jObject["info"]["title"].Value<string>()
+                Title = jObject["info"]["title"].Value<string>(),
+                Definitions = actions
             };
         }
     }
